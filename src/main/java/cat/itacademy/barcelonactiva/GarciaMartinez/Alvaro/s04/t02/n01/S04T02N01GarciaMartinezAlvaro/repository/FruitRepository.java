c16e4273991c0ebd9fb84131ad8d195c8f4b55fa
AlvaroGarcia3006 <113432141+AlvaroGarcia3006@users.noreply.github.com>
2023-03-08 11:56:19 +0100

    v2

--- src/main/java/cat/itacademy/barcelonactiva/GarciaMartinez/Alvaro/s04/t02/n01/S04T02N01GarciaMartinezAlvaro/repository/FruitRepository.java
+++ src/main/java/cat/itacademy/barcelonactiva/GarciaMartinez/Alvaro/s04/t02/n01/S04T02N01GarciaMartinezAlvaro/repository/FruitRepository.java
@@ -2,6 +2,5 @@ package cat.itacademy.barcelonactiva.GarciaMartinez.Alvaro.s04.t02.n01.S04T02N01
 
 import cat.itacademy.barcelonactiva.GarciaMartinez.Alvaro.s04.t02.n01.S04T02N01GarciaMartinezAlvaro.domain.Fruit;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 public interface FruitRepository extends JpaRepository<Fruit, Integer> {
 }
